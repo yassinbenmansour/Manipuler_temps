e63f5ac8c1fa4b54281de10aaaacb4bba170c08c
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-03-25 20:29:16 +0100

    Update app.js

--- manipulation du temps/js/app.js
+++ manipulation du temps/js/app.js
@@ -1,20 +1,6 @@
 var d = new Date("2022-03-25T20:00:00Z");
 
-//console.log(d);
-//d.setFullYear(20223);
 
-//console.log(d);
-//console.log(d.getDate());
-//console.log(d.getMonth() + 1); // Mois de 0 à 11
-//console.log(d.getFullYear());
-//console.log(d.getHours());
-//console.log(d.getMinutes());
-//console.log(d.getSeconds());
-//console.log(d.getMilliseconds());
-
-//console.log(d.toString());
-//console.log(d.toUTCString());
-//console.log(d.toDateString());​
 
 function refreshTime() {
     var tempsCourant = document.getElementById("temps-actuel");
